6f750ac78a880cbbf266fba43084b97ecd83337b
mariana9uk <maryanagolyuk@gmail.com>
2023-06-29 21:02:22 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,3 +10,20 @@ player.on('play', function() {
 player.getVideoTitle().then(function(title) {
     console.log('title:', title);
 })
+
+player.on('timeupdate', function(data) {
+    const current = data.seconds;
+    console.log(current);
+    localStorage.setItem("videoplayer-current-time", JSON.stringify(current));
+})
+// window.addEventListener('load', function(e){
+//     e.preventDefault()
+//     const savedTime = localStorage.getItem("videoplayer-current-time");
+//     const parsedSavedTime = JSON.parse(savedTime)
+//     player.setCurrentTime(parsedSavedTime).then(seconds);
+// })
+window.addEventListener('load', function(e) {
+    e.preventDefault()});
+    const savedTime = localStorage.getItem("videoplayer-current-time");
+     const parsedSavedTime = JSON.parse(savedTime);
+player.setCurrentTime(parsedSavedTime)
