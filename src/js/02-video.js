ddcf97c6ad1e41ab7151008feb37cefdebcae42d
mariana9uk <maryanagolyuk@gmail.com>
2023-06-29 12:08:37 +0300

    task2 draft

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
 
+import Vimeo from '@vimeo/player'
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+})
